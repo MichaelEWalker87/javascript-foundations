dc41b903bf92a8a889c4ecbcb7cc99eb0e5d2b5e
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-14 16:06:05 -0600

    Pass all test to magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,7 +1,41 @@
+var spell = 0
 class Magician {
-  constructor() {
+  constructor(magicianStuff) {
+    this.name = magicianStuff.name;
+    this.topHat = magicianStuff.topHat;
+    this.confident = false;
 
+    if (magicianStuff.topHat === undefined){
+      this.topHat = magicianStuff.topHat = true
+    } else {
+        magicianStuff.topHat = magicianStuff.topHat
+    }
+  }
+
+  incantation(message){
+    return `${message.toUpperCase()}!`;
+  }
+
+  cast(){
+    spell++
+    if (spell >= 3){
+      this.confident = true;
+    }
+    if ( this.topHat === true){
+      return 'PULL RABBIT FROM TOP HAT';
+    } else {
+      return 'PULL DOVE FROM SLEEVE';
+    }
+  }
+
+  performShowStopper(){
+    if (this.confident === false) {
+      return 'Oh no! Practice more before attempting this trick!'
+    } else {
+      return 'WOW! The magician totally just sawed that person in half!'
+    }
   }
 }
 
-module.exports = Magican;
+
+module.exports = Magician;
